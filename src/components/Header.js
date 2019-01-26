b3dd55f4f00c5779c1b736337b30a8e19a9fc7de
Hamza Hawkins <hhawkins@users.noreply.github.com>
2019-01-26 17:39:43 -0500

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import Footer from './Footer'
+import avatar from '../assets/images/myavatar.png'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header id="header">
+                <div className="inner">
+                    <a href="https://www.hamzahawkins.com"><h1><strong>HAMZA HAWKINS</strong></h1></a>
+                    
+                    <ul>
+                        <li><a href="#" className="header-links">(coming soon)My Story</a></li>
+                        <li><a href="#" className="header-links">(coming soon)My Background</a></li>
+                        <li><a href="#" className="header-links">(coming soon)My Current Endeavors</a></li>
+                    </ul>
+                </div>
+                <Footer />
+            </header>
+        )
+    }
+}
+
+export default Header
